04599d32ee4926771d3a5467f53f0d1097494066
TrucHuynh2002 <nguyentruchuynh@nl@gmail.com>
2022-10-17 16:42:15 +0700

    update menu responsive

---
+++ admin/asset/js/menu.js
@@ -0,0 +1,6 @@
+const getMenu = document.querySelector(".navbar");
+const getMenuIcons = document.querySelector(".menu");
+getMenuIcons.addEventListener("click", function(){
+    getMenu.classList.toggle("tokenMenu")
+
+})
