f82127413229690c521d18f8880f94df7eb4a4ab
VanVu2002 <ngovanvu2002@gmail.com>
2022-11-18 16:31:59 +0700

    thaydoiroine

--- fe-datn/src/components/admin/category/AddCategory.js
+++ fe-datn/src/components/admin/category/AddCategory.js
@@ -1,56 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap';
 
 
 
-//thêm danh mục 
-// var CourseApi = 'http://127.0.0.1:8000/api/category/create'; 
-// function start(){
-// handleCreateForm();
 
-// };
-// start();
-
-
-// function handleCreateForm(){
-//   var createCategory = document.querySelector('#create');
-//   createCategory.onclick = function() {
-//     var name_category = document.querySelector('#name_category').value;
-    
-//     var data = {
-//         name_category: name_category
-//     };
-//     var options =
-//     {
-//         method:"POST",
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: data
-//     }
-//     fetch(CourseApi,options)
-//     .then(function(response){ return response.data})
-//     .then(function(data){ return console.log(data)})
-//   }
-// }
 
 
 function AddCategory() {
+    const [name, setName] = useState('')
+    const handleSubmit = () => {
+        console.log({
+            name
+        })
+    }
+
+
   return (
     <div className="content">
         <div className="add-post">
-            <h1 style={{ textAlign: "center", padding: "5px", color: "#0d3380" }}>Thêm danh mục </h1>
-              <Form>
-                  <Form.Group className="mb-3" controlId="name_category">
-                      <Form.Label>Tên danh mục</Form.Label>
-                      <Form.Control type="text" name="name_category" className=''/>
-                  </Form.Group>
-                  <div className="d-grid gap-2">
-                      <Button variant="primary" size="sm" name='' type="submit">
-                          Thêm danh mục
-                      </Button>
-                  </div>
-              </Form>     
+            <h1 style={{ textAlign: "center", padding: "5px", color: "#0d3380" }}>Thêm danh mục</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="name_roomType">
+                    <Form.Label>Tên danh mục </Form.Label>
+                    <Form.Control 
+                     value={name}
+                    type="text"
+                     name="name_roomType" 
+                     className=''
+                      onChange = {e => setName(e.target.value)}
+                      
+                      />
+                    
+                </Form.Group>
+                <div className="d-grid gap-2">
+                    <Button
+                    onClick={handleSubmit}
+                   >
+                        Thêm danh mục
+                    </Button>
+                </div>
+            </Form>
         </div>
     </div>
   )
