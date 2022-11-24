d8a5cc5930b53979b6fa7ef7260c936eb0656823
VanVu2002 <ngovanvu2002@gmail.com>
2022-11-24 17:39:49 +0700

    chao

---
+++ fe-datn/src/components/user/Header_menu.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import HeaderNavLink from './HeaderNavLink';
+
+function Header_menu() {
+  return (
+    <div className="col-xl-10 col-lg-10 col-md-10 col-sm-10">
+              <nav className="navigation navbar navbar-expand-md navbar-dark ">
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarsExample04"
+                  aria-controls="navbarsExample04"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon" />
+                </button>
+                <HeaderNavLink />
+              </nav>
+            </div>
+  )
+}
+
+export default Header_menu
