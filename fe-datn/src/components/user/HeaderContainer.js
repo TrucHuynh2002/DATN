7ba1c3ad7ad862c80b4043d2b28ca3459972127d
Luonghoangphat <97626114+Luonghoangphat@users.noreply.github.com>
2022-11-24 23:53:14 +0700

    chao

--- fe-datn/src/components/user/HeaderContainer.js
+++ fe-datn/src/components/user/HeaderContainer.js
@@ -4,15 +4,9 @@ import Header_menu from './Header_menu'
 function HeaderContainer() {
   return (
     <>
-<<<<<<< HEAD
      <div className="row header-nav">
           <Header_logo />
           <Header_menu />
-=======
-      <div className="row header-nav">
-        <Header_logo />
-        <Header_menu />
->>>>>>> 212bea098b95479b0442ecd824e4b28dedcdb787
       </div>
     </>
   )
