ea894590a212ae7f4085966b02db54730cbdac73
Luonghoangphat <97626114+Luonghoangphat@users.noreply.github.com>
2022-11-25 00:16:26 +0700

    chuong

--- fe-datn/src/components/user/HeaderNavLink.js
+++ fe-datn/src/components/user/HeaderNavLink.js
@@ -31,16 +31,38 @@ function HeaderNavLink() {
                 Blog
                 </Link>
             </li>
-           
             <li className="nav-item">
                 <Link className="nav-link" to="contact">
                 Liên hệ
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="addpost">
-                Đăng bài
-                </Link>
+                <div class="btn-group" >
+                   <div data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" 
+                        style={{color: 'black', fontSize:'1.8em',border: 'none'}} >
+                        <i class='bx bx-bell'></i>
+                    </div>
+                    <div class="dropdown-menu" style={{zIndex:"1001",padding:"10px"}}>
+                        <ul class="aw__t1bz0i0v" aria-label="notification" role="tablist">
+                            <li class="aw__tfaflwg">
+                                <span class="aw__i1mg2bze" tabindex="-1" role="tab">HOẠT ĐỘNG</span>
+                            </li>
+                        </ul>
+                        <div class="aw__t16jo35">
+                            <div>Vui lòng đăng nhập để xem danh sách hoạt động.
+                                <Link class="dropdown-item nav-link btn btn-warning" style={{color: 'black', fontWeight: 600,borderRadius: '5px'}} to="login">Đăng nhập</Link>
+                                <Link class="dropdown-item nav-link btn btn-warning" style={{color: 'black', fontWeight: 600,borderRadius: '5px'}} to="signin">Đăng ký</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="nav-item">
+                <div class="btn-group">
+                    <Link className="nav-link btn btn-warning" style={{color: 'black', fontWeight: 600, backgroundColor: '#ffc70d',borderRadius: '5px'}} to="addpost">
+                    Đăng bài
+                    </Link>
+                </div>
             </li>
             <li class="nav-item">
                 <div class="btn-group">
@@ -50,9 +72,8 @@ function HeaderNavLink() {
                         <Link class="dropdown-item" to="signin">Đăng ký</Link>
                     </div>
                 </div>
-              </li>
-
-            </ul>
+            </li>
+        </ul>
         {/* </div> */}
     </>
   )
