e405d7c82df052efb531c7764a9d1e43867e160e
trienpq123 <anhtrienpq123@gmail.com>
2022-11-25 21:34:35 +0700

    Check admin Perfect

--- fe-datn/src/components/admin/App.js
+++ fe-datn/src/components/admin/App.js
@@ -4,17 +4,45 @@ import { Outlet, navigator, useNavigate } from 'react-router-dom';
 import NavAdmin from './NavAdmin';
 import HeaderAdmin from './HeaderAdmin';
 import FooterAdmin from './FooterAdmin';
+import axios from 'axios';
 
 function App() {
   const navigate = useNavigate();
-  useEffect(() => {
+  const checkAdmin = async () => {
     const get_user = JSON.parse(localStorage.getItem('user'));
-    console.log(get_user[0].id);
+    const res = await axios.get("http://127.0.0.1:8000/api/user/show/"+get_user[0].id);
     if(get_user){
-      if(get_user[0].role == 0 ){
+      if(res.data.status == true){
+        const user_data = res.data.data;
+        console.log(user_data); 
+        if(user_data.role == 0){
+          navigate('/');
+        }
+      }else{
         navigate('/');
       }
+    }else{
+      navigate('/');
     }
+   
+  }
+    // console.log(get_user[0].id)
+    
+    // const id_user = get_user[0].id
+    // console.log(id_user)
+
+      // if(get_user){
+    //   if(get_user[0].role == 0 ){
+    //     navigate('/');
+    //   }
+    // }
+  
+  useEffect(() => {
+
+    checkAdmin();
+
+    
+   
   },[])
   return (
     <>
