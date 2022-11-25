9fa61b5541ae3d9ae68e1736cb25343f5ffe0691
trienpq123 <anhtrienpq123@gmail.com>
2022-11-25 21:04:05 +0700

    Middleware Login Admin

--- fe-datn/src/components/admin/App.js
+++ fe-datn/src/components/admin/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Container, Row } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
+import { Outlet, navigator, useNavigate } from 'react-router-dom';
 import NavAdmin from './NavAdmin';
 import HeaderAdmin from './HeaderAdmin';
 import FooterAdmin from './FooterAdmin';
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const get_user = JSON.parse(localStorage.getItem('user'));
+    console.log(get_user[0].id);
+    if(get_user){
+      if(get_user[0].role == 0 ){
+        navigate('/');
+      }
+    }
+  },[])
   return (
     <>
       <Container fluid>
