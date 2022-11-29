e1f511e5f0f9d75a07140c52df0e27cea42d78ac
Luonghoangphat <97626114+Luonghoangphat@users.noreply.github.com>
2022-11-30 00:37:22 +0700

    update useeeerrr

--- fe-datn/src/components/user/account/EditAvata.js
+++ fe-datn/src/components/user/account/EditAvata.js
@@ -13,14 +13,9 @@ function EditAvata() {
                                 <div className='row'>
                                     <div className='col-md-12'>
                                     <Form>
-<<<<<<< HEAD
                                         <Form.Group className="mb-3" controlId="logo">
                                             <h3 className="content_h1_admin"><b>Cập nhật ảnh đại diện</b></h3>
                                             {/* <Form.Label>Avata</Form.Label> */}
-=======
-                                        <Form.Group className="mb-3" controlId="avata">
-                                            <h3 style={{textAlign:"center", margin:"20px", fontSize:"20px"}}><b>Cập nhật ảnh đại diện</b></h3>
->>>>>>> 0181eaf90ded422a7086019354fb39e259684a36
                                             <Form.Control type="file" name="avata" className=''/>
                                         </Form.Group>
                                         <Button variant="primary" className='' name="" type="submit">Cập nhật</Button>   
