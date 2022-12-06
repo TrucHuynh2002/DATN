9d5f4cf7d59f7421ba064ce5cd7866de8ca8dcb7
TrucHuynh2002 <nguyentruchuynh@nl@gmail.com>
2022-12-07 02:36:21 +0700

    category

--- fe-datn/src/components/admin/category/EditCategory.js
+++ fe-datn/src/components/admin/category/EditCategory.js
@@ -9,13 +9,14 @@ function EditCategory() {
     const {id_category} = useParams();
     const [editCategory, setEditCategory] = useState({
         name_category: "",
+        link_to: "",
     });
 
     const [alert, setAlert] = useState({
         err_list: {},
     });
 
-    const { name_category } = editCategory;
+    const { name_category, link_to } = editCategory;
 
     const handleChange = (e) => {
         setEditCategory({ ...editCategory, [e.target.name]: e.target.value });
@@ -57,9 +58,12 @@ function EditCategory() {
                     <Form.Label>Tên danh mục</Form.Label>
                     <Form.Control type="text" name="name_category" onChange={(e) => handleChange(e)}
                     value={name_category} className=''/>
-
-                        {/* Thông báo  */}
                         {alert.err_list.status === false && <div className="notice warning_____ ">{alert.err_list.messages.name_category[0]}</div>}    
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="link_to">
+                    <Form.Label>Link to</Form.Label>
+                      <Form.Control type="text" onChange={(e) => handleChange(e)} value={link_to} name="link_to" className=''/>
+                      {alert.err_list.status === false && <div className="notice warning_____ ">{alert.err_list.messages.link_to[0]}</div>}                   
                 </Form.Group>
                 <div className="d-grid gap-2">
                 {alert.err_list.status === true && <div className="notice success_____">Cập nhật thành công</div>}
