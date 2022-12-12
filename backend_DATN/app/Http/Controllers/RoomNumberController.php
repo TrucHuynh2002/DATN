43c0173a8f7bc52403573d45eeccc108f035a6e0
andreistony <andreistony17@gmail.com>
2022-12-12 21:06:05 +0700

    controller model noty number

--- backend_DATN/app/Http/Controllers/RoomNumberController.php
+++ backend_DATN/app/Http/Controllers/RoomNumberController.php
@@ -15,9 +15,10 @@ class RoomNumberController extends Controller
                 'status' => true
             ]);
     }
-    public function show_one(Request $request, $id_post)
+    public function show_one(Request $request, $id)
     {
-        $data = RoomNumberModel::where('id','=',$id_post)->get();
+        // $data = RoomNumberModel::where('id','=',$id)->get();
+        $data = RoomNumberModel::find($id);
         return response()
             ->json([
                 'data' => $data,
@@ -27,7 +28,9 @@ class RoomNumberController extends Controller
     public function update(Request $request, $id)
     {
         $data = RoomNumberModel::find($id);
-        
+        $data->room_number = $request->room_number;
+        $data->status = $request->status;
+        $data->save();
         return response()
             ->json([
                 'data' => $data,
