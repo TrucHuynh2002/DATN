f144885b4e6fa65c0b9b1955d34996e7c5163906
andreistony <andreistony17@gmail.com>
2022-12-15 18:09:14 +0700

    ddddd

--- backend_DATN/app/Http/Controllers/ViewController.php
+++ backend_DATN/app/Http/Controllers/ViewController.php
@@ -4,21 +4,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\View;
+use Carbon\Carbon;
 
 class ViewController extends Controller
 {
     public function updateViewIndex()
     {
-        $a = View::all();
-        foreach ($a as $val) {
-            $view = $val['view_index'];
-        };
-        // $update = View::update(['view_index' => $view + 1]);
-        // return response()
-        //     ->json([
-        //         'data' =>  $update,
-        //         'status' => true
-        //     ]);
-        return $view;
+        // $a = View::all();
+        // foreach ($a as $val) {
+        //     $view = $val['view_index'];
+        // };
+        // return $view;
+        // $update = View::updated('view_index','=','view_index'+1);
+        // $update;
+        
+        $data = View::orderBy('id_view_index','DESC')->first();
+        dd($data->updated_at->day);
+        if($data->updated_at->day == Carbon::now('Asia/Ho_Chi_Minh')->day){
+            $data->view_index++;
+            $data->save();
+            return response()
+            ->json([
+                'data' =>  $data,
+                'status' => true
+            ]);
+        }else{
+            $data_new = new View();
+            $data_new->view_index++;
+            $data_new->save();
+            return response()
+            ->json([
+                'messeges' =>  'ngày mới',
+                'data' =>  $data_new,
+                'status' => true
+            ]);
+        }
+
+        // $data = View::find(1);
+        // $data->view_index++;
+        // $data->save();
+        
+        
+    }
+    public function show()
+    {
+        $data = View::all();
+        return response()
+            ->json([
+                'data' =>  $data,
+                'status' => true
+            ]);
     }
 }
