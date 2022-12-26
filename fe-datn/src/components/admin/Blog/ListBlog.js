9df62476f0a4670142e944f7b397a9c950e5274f
TrucHuynh2002 <nguyentruchuynh@nl@gmail.com>
2022-12-26 16:35:11 +0700

    ff

--- fe-datn/src/components/admin/Blog/ListBlog.js
+++ fe-datn/src/components/admin/Blog/ListBlog.js
@@ -28,85 +28,18 @@ function ListBlog() {
     getData();
   };
   // search
-  // const [keyword,setKeyword] = useState({
-  //   keywords: ""
-  // })
-  // const {
-  //   keywords,
-  // } = keyword
-  const [searching,setSearching] = useState(false);
-          const [getKeywords,setgetKeywords] = useState([]);
-          const [getDataPostSearch,setGetDataPostSearch] = useState([]);
-          // const getKeyword = async (keyword) => {
-          //   const res = await axios.get(`${url}/getkeywordblog/${keyword}`);
-          //   console.log(res.data)
-          //   setgetKeywords(res.data.data)
-          //   setGetDataPostSearch(res.data.get_category)
-          // }
-          const handleChangeKeyWord = (e) => {
-            // setKeyword({ ...keyword,[e.target.name]:e.target.value});
-            getData(e.target.value)
-
-            // if(e.target.value.length > 0){
-            //   getKeyword(e.target.value)
-            // }else{
-            //   setSearching(false)
-            // }
-          }
-          const handleSubmitSearch = (e) => {
-            e.preventDefault()
-          }
+    const handleChangeKeyWord = (e) => {
+      getData(e.target.value)
+    }
 
   return (
     <div className="content">
             <div className="add-post">
               <h1 className="content_h1_admin">Danh sách Blog</h1>
               {/* start search */}
-              <form onSubmit={(e) => handleSubmitSearch(e)}>
                 <div className='row'>
                     <input className="form-control search_blog" placeholder="Tìm kiếm" type="text" name="keywords" onChange={(e) => handleChangeKeyWord(e)} />
-                    {searching &&  (
-                              <div className='show_search'>
-                                 <ul>
-                                  {
-                                    getDataPostSearch.length > 0
-                                    &&
-                                   getDataPostSearch.map((post,index) => {
-                                      return (
-                                        <li key={index}>
-                                              <Link to={`../blogdetail/${post.id_blog}`}>{post.name_blog}</Link>                              
-                                        </li>
-
-                                      )
-                                   })
-
-                                  }
-                            
-                                  {
-                                    getKeywords.length > 0 
-                                    &&
-                                    getKeywords.map((keyword,index) => {
-                                      return (
-                                        <li key={index}>
-                                          <Link to="room">{keyword.key_word}</Link>
-                                        </li>
-                                      )                                          
-                                    })                                  
-                                  }
-                                    {/* <li>
-                                      <Link to={`searchroom?keyword=${keywords}`}>Tìm kiếm với {keywords}</Link>
-                                    </li> */}
-                                </ul>               
-                              </div>    
-                            )
-                          }
-                    <div className="btn-search col-1">
-                      <button className="btn btn-outline-secondary">
-                        <i className='bx bx-search' style={{color:"#0d3380"}}></i>
-                      </button>
-                    </div>
                 </div>
-              </form>
               {/* end search */}
               <Link to="../add_blog" className="btn btn-primary form-add">Thêm Blog</Link>
               <Table bordered>
